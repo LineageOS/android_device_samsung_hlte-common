0c6fbc49ccba6daf6b4a75f6734da5c59efd2c7b
Shareef Ali <shareefalis@gmail.com>
2013-10-24 20:53:08 -0400

    audio fixes for calls

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,8 +93,7 @@ BOARD_HAVE_DOCK_USBAUDIO := true
 # Use the ES325 dual mic feature
 BOARD_HAVE_AUDIENCE_ES325_2MIC := true
 
-# Samsung's nonstandard csd-client
-BOARD_HAVE_SAMSUNG_CSDCLIENT := true
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
 
 # Use seperate devices for VOIP
 BOARD_USES_SEPERATED_VOIP := true
