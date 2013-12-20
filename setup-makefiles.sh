41e6e6613c3c20303dc9a6bae812ced0f70c4697
Shareef Ali <shareefalis@gmail.com>
2013-12-20 16:57:34 -0600

    fixup build error

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -200,12 +200,12 @@ EOF
 
 LOCAL_PATH := \$(call my-dir)
 
-ifneq (\$(filter jflteatt jfltespr jfltetmo jfltevzw jfltexx jfltecan jflteusc jfltecri jfltecsp jfltezm,\$(TARGET_DEVICE)),)
+ifneq (\$(filter hltexx, \$(TARGET_DEVICE)),)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/lib/libtime_genoff.so
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
