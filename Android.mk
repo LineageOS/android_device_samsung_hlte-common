67402f45f8127e728b0e7129391c5588dceccf8d
slayher <layhertony@gmail.com>
2013-12-25 22:29:46 -0500

    Fix Derpage
    Change-Id: Idccbb126fa9325fb579aee194af3bcccd042aaaa

--- Android.mk
+++ Android.mk
@@ -24,7 +24,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
+
 ifneq ($(filter hltexx hltetmo hltespr,$(TARGET_DEVICE)),)
+
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
 endif
