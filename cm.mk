495a377f51609bac4187ea3cb80b0e7caf684821
slayher <layhertony@gmail.com>
2014-02-01 22:09:41 -0500

    hlte: [WIP] utilize INIT_VENDOR_LIB TODO: make sure bootloader values correct for all devices
    Change-Id: If7eb32fd206b7a5bab143d3ac77b963d370deab9

--- cm.mk
+++ cm.mk
@@ -9,12 +9,5 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=hlte \
-    TARGET_DEVICE=hlte \
-    BUILD_FINGERPRINT="xxxxxxxxxx" \
-    PRIVATE_BUILD_DESC="xxxxxxxxxx"
-
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := cm_hlte
