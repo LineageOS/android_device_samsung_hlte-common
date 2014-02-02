e1535073e45c81cc07c31c0ed31d5e2142e68e56
slayher <layhertony@gmail.com>
2014-02-01 22:09:41 -0500

    hlte: [WIP] utilize TARGET_UNIFIED_DEVICE
    TODO: make sure bootloader values correct for all devices
    DONE: Handle ril libs.
    PS8: Add VZW required properties.
    
    Change-Id: If7eb32fd206b7a5bab143d3ac77b963d370deab9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,3 +125,10 @@ TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_eur_defconfig
 
 # PowerHAL extension
 TARGET_POWERHAL_SET_INTERACTIVE_EXT := device/samsung/hlte/power/power_ext.c
+
+# Vendor Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/hlte/init/init_hlte.c
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/hlte
