b51a668eb5a29dce4d6668f65fa0251e512e0bb0
Steve Kondik <shade@chemlab.org>
2014-03-01 10:17:29 +0100

    hlte: Enable hardware keystore
     * Also a few SELinux related fixes.
    
    Change-Id: Ibebbbdbf62d997d7d23c16e64eee3f645cf93c1b

--- device.mk
+++ device.mk
@@ -219,6 +219,10 @@ PRODUCT_COPY_FILES += \
    device/samsung/hlte/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
    device/samsung/hlte/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    keystore.msm8974
+
 # call common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
 
