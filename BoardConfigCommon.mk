0221ede7399068e29966befac6593be849f5b92c
Ethan Chen <intervigil@gmail.com>
2014-09-11 14:47:03 -0700

    Revert "hlte: enable userspace vibration strength control (2/2)"
    * Use existing common class in hardware/samsung/cmhw instead.
    
    This reverts commit 524bd3cd4e9738a4d91047522771acd7f9a39c8f.
    
    Change-Id: Ie777e3b88f4b4e49be42b9e1b51f1e29b943c04c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,3 @@ WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin 
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
-
-# Hardware
-BOARD_HARDWARE_CLASS := device/samsung/hlte-common/cmhw
