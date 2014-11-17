62faf250974eb6526253193065995450b9680706
Tony Layher <layhertony@gmail.com>
2014-11-17 13:45:25 -0500

    hlte-common: Add CameraParametersExtra.h for wrapper.
    Change-Id: I81c76ab12bfff700b625514ae5c6858d8871dccf

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -18,6 +18,21 @@
 const char CameraParameters::KEY_DIS[] = "dis"; \
 const char CameraParameters::DIS_DISABLE[] = "disable"; \
 \
+const char CameraParameters::KEY_DYNAMIC_RANGE_CONTROL[] = "dynamic-range-control"; \
+const char CameraParameters::KEY_SUPPORTED_DYNAMIC_RANGE_CONTROL[] = "dynamic-range-control-values"; \
+const char CameraParameters::DRC_ON[] = "on"; \
+const char CameraParameters::DRC_OFF[] = "off"; \
+\
+const char CameraParameters::KEY_PHASE_AF[] = "phase-af"; \
+const char CameraParameters::KEY_SUPPORTED_PHASE_AF[] = "phase-af-values"; \
+const char CameraParameters::PAF_ON[] = "on"; \
+const char CameraParameters::PAF_OFF[] = "off"; \
+\
+const char CameraParameters::KEY_RT_HDR[] = "rt-hdr"; \
+const char CameraParameters::KEY_SUPPORTED_RT_HDR[] = "rt-hdr-values"; \
+const char CameraParameters::RTHDR_ON[] = "on"; \
+const char CameraParameters::RTHDR_OFF[] = "off"; \
+\
 const char CameraParameters::KEY_ZSL[] = "zsl"; \
 const char CameraParameters::ZSL_ON[] = "on"; \
 const char CameraParameters::ZSL_OFF[] = "off"; \
@@ -31,6 +46,21 @@ int CameraParameters::getInt64(const char *key) const { return -1; } \
 #define CAMERA_PARAMETERS_EXTRA_H \
     static const char KEY_DIS[]; \
     static const char DIS_DISABLE[]; \
+    \
+    static const char KEY_DYNAMIC_RANGE_CONTROL[]; \
+    static const char KEY_SUPPORTED_DYNAMIC_RANGE_CONTROL[]; \
+    static const char DRC_ON[]; \
+    static const char DRC_OFF[]; \
+    \
+    static const char KEY_PHASE_AF[]; \
+    static const char KEY_SUPPORTED_PHASE_AF[]; \
+    static const char PAF_ON[]; \
+    static const char PAF_OFF[]; \
+    \
+    static const char KEY_RT_HDR[]; \
+    static const char KEY_SUPPORTED_RT_HDR[]; \
+    static const char RTHDR_ON[]; \
+    static const char RTHDR_OFF[]; \
     \
     static const char KEY_ZSL[]; \
     static const char ZSL_ON[]; \
