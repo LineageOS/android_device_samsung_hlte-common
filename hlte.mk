834b57f12534324193796a7f3657c1bec244639a
Tony Layher <layhertony@gmail.com>
2014-11-23 14:18:41 -0500

    hlte-common: More wifi updates for L
    Change-Id: I86f2ca0549a5fb6760382135499152c11aa507e7

--- hlte.mk
+++ hlte.mk
@@ -121,11 +121,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnetcmdiface \
     macloader \
+    dhcpcd.conf \
     hostapd \
+    hostapd_default.conf \
     libwpa_client \
     wpa_supplicant
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
