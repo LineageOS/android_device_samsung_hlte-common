488be0a56ecacb4a8b90a1b5fc935c3685ff2c37
repo Shareef Ali <shareefalis@gmail.com>
2014-12-29 19:11:35 -0600

    hlte: enable hw aac encoder
    Change-Id: If1c12be4819a0927e084f6eb2665712c0da1a4d8

--- system_prop.mk
+++ system_prop.mk
@@ -15,7 +15,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     tunnel.audio.encode=true \
     media.aac_51_output_enabled=true \
     audio.offload.pcm.16bit.enable=true \
-    audio.offload.pcm.24bit.enable=true
+    audio.offload.pcm.24bit.enable=true  \
+    qcom.hw.aac.encoder=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
