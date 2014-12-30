4e47c940ee5a9d9b803d0dd0aea5d52bc77e8c6c
repo Shareef Ali <shareefalis@gmail.com>
2014-12-29 21:27:38 -0600

    hlte: remove lolipop hack
    Change-Id: I3f58cd3be10a1296c47f520a3311591d8ab1db8e
    require: http://review.cyanogenmod.org/#/c/83032/

--- ril/telephony/java/com/android/internal/telephony/hlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/hlteRIL.java
@@ -639,28 +639,4 @@ public class hlteRIL extends RIL implements CommandsInterface {
         send(rr);
     }
 
-    // Hack for Lollipop
-    // The system now queries for SIM status before radio on, resulting
-    // in getting an APPSTATE_DETECTED state. The RIL does not send an
-    // RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED message after the SIM is
-    // initialized, so delay the message until the radio is on.
-    @Override
-    public void
-    getIccCardStatus(Message result) {
-        if (mState != RadioState.RADIO_ON) {
-            mPendingGetSimStatus = result;
-        } else {
-            super.getIccCardStatus(result);
-        }
-    }
-
-    @Override
-    protected void switchToRadioState(RadioState newState) {
-        super.switchToRadioState(newState);
-
-        if (newState == RadioState.RADIO_ON && mPendingGetSimStatus != null) {
-            super.getIccCardStatus(mPendingGetSimStatus);
-            mPendingGetSimStatus = null;
-        }
-    }
 }
