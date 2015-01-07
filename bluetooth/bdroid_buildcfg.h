936c34d39c53466dff806d83ac972e5ddf8d5017
repo Shareef Ali <shareefalis@gmail.com>
2015-01-07 09:13:32 -0600

    hlte: actually fix lpm.
    * kernel has a timeout of 5  second.
    * add bluetooth features
    * It is perfect now
    
    Change-Id: Idc95e5aa0555056fc591ba0371c2268957f56b91

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,4 +19,9 @@
 
 #define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Note 3"
 
+#define BTA_DISABLE_DELAY 100 /* in milliseconds */
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+#define BLE_VND_INCLUDED TRUE
+
 #endif
