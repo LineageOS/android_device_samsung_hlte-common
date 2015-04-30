dd87716799b385abb7ad8e15bf04d18ffeea3d1b
Patrick Lower <devvortex@gmail.com>
2015-04-30 11:09:17 -0400

    MMS: Add property to override RIL dataProfileId
    For sending and receiving MMS on carriers with separate mms apn
    
    Change-Id: I7666b8775ce2b37f2e47c7fb5235afeb0c875a37

--- system_prop.mk
+++ system_prop.mk
@@ -56,6 +56,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.add_power_save=1 \
     rild.libpath=/system/vendor/lib/libsec-ril.so \
+    ro.telephony.mms_data_profile=5 \
     ro.ril.telephony.mqanelements=6
 
 # Sensors
