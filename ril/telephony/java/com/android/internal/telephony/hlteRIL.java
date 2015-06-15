c98a32e8b31a4fd8930c6f2dcf39dfafde4f2b47
Shareef Ali <shareefalis@cyanogenmod.org>
2015-06-15 18:47:34 -0400

    hlte : fix e911 issue, match data structure that Samsung message interface want.
    taken from: https://github.com/CyanogenMod/android_device_samsung_hlte/blob/cm-11.0/ril/telephony/java/com/android/internal/telephony/HlteRIL.java
    
    Change-Id: If60fa6090a64521e829580021c61a512db5e8ec3
    (cherry picked from commit c8c2c37f15f9f4e578843b22832ad6ef52adc85b)
    (cherry picked from commit a9b91333293c9d3aed710bb991ed4a5ab3ade931)

--- ril/telephony/java/com/android/internal/telephony/hlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/hlteRIL.java
@@ -404,6 +404,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
     }
 
     static final int RIL_REQUEST_DIAL_EMERGENCY = 10016;
+
     private void
     dialEmergencyCall(String address, int clirMode, Message result) {
         RILRequest rr;
@@ -412,13 +413,16 @@ public class hlteRIL extends RIL implements CommandsInterface {
         rr = RILRequest.obtain(RIL_REQUEST_DIAL_EMERGENCY, result);
         rr.mParcel.writeString(address + "/");
         rr.mParcel.writeInt(clirMode);
-        rr.mParcel.writeInt(0);  // UUS information is absent
+        rr.mParcel.writeInt(0);        // CallDetails.call_type
+        rr.mParcel.writeInt(3);        // CallDetails.call_domain
+        rr.mParcel.writeString("");    // CallDetails.getCsvFromExtra
+        rr.mParcel.writeInt(0);        // Unknown
 
         if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
 
         send(rr);
     }
-    
+
     @Override
     public void
     acceptCall (Message result) {
