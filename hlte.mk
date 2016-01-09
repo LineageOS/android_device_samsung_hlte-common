61fcd3aa6f3c6b5a716f5f57955da3c536482942
Ethan Chen <intervigil@gmail.com>
2016-01-09 09:47:31 +0100

    hlte-common: Add RIL shim library
    Change-Id: I42ee22a73cfce121a391511c61e6acc2bbcc83fa

--- hlte.mk
+++ hlte.mk
@@ -112,6 +112,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/libnfc-brcm-20791b04.conf:system/etc/libnfc-brcm-20791b04.conf \
     $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
+# Radio
+PRODUCT_PACKAGES += \
+    libril_shim
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
