a975b5ec66a626bbf2d5fdc762917447b003addc
B--B <dark.schneider.ds@gmail.com>
2016-01-12 01:45:13 +0100

    ril: Fix emergency call
    Remove dialEmergencyCall; it causes an NPE every time
    the user dials an emergency number.
    
    Change-Id: I91f8abe0ea509ec6eaf18ab94ae808c74e904ba6

--- ril/telephony/java/com/android/internal/telephony/hlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/hlteRIL.java
@@ -60,7 +60,6 @@ public class hlteRIL extends RIL implements CommandsInterface {
     private boolean mIsSendingSMS = false;
     protected boolean isGSM = false;
 
-    private static final int RIL_REQUEST_DIAL_EMERGENCY = 10001;
     public static final long SEND_SMS_TIMEOUT_IN_MS = 30000;
 
     public hlteRIL(Context context, int preferredNetworkType, int cdmaSubscription) {
@@ -547,10 +546,6 @@ public class hlteRIL extends RIL implements CommandsInterface {
     @Override
     public void
     dial(String address, int clirMode, UUSInfo uusInfo, Message result) {
-        if (PhoneNumberUtils.isEmergencyNumber(address)) {
-            dialEmergencyCall(address, clirMode, result);
-            return;
-        }
         RILRequest rr = RILRequest.obtain(RIL_REQUEST_DIAL, result);
 
         rr.mParcel.writeString(address);
@@ -617,24 +612,6 @@ public class hlteRIL extends RIL implements CommandsInterface {
                 result.sendToTarget();
             }
         }
-    }
-
-    private void
-    dialEmergencyCall(String address, int clirMode, Message result) {
-        RILRequest rr;
-        Rlog.v(RILJ_LOG_TAG, "Emergency dial: " + address);
-
-        rr = RILRequest.obtain(RIL_REQUEST_DIAL_EMERGENCY, result);
-        rr.mParcel.writeString(address + "/");
-        rr.mParcel.writeInt(clirMode);
-        rr.mParcel.writeInt(0);        // CallDetails.call_type
-        rr.mParcel.writeInt(3);        // CallDetails.call_domain
-        rr.mParcel.writeString("");    // CallDetails.getCsvFromExtra
-        rr.mParcel.writeInt(0);        // Unknown
-
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
-
-        send(rr);
     }
 
     // This call causes ril to crash the socket, stopping further communication
