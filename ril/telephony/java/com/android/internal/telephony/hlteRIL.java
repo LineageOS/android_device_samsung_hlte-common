2a81038a266f313c7ee7f5a24954d39b1776ae52
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2016-09-15 10:20:16 +0200

    hlte-common: update wakelocks semantics
    Change-Id: Ia82bcfe56049f7e5cd897b5c48e37d2dbf461e87

--- ril/telephony/java/com/android/internal/telephony/hlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/hlteRIL.java
@@ -39,6 +39,7 @@ import com.android.internal.telephony.cdma.CdmaInformationRecords.CdmaSignalInfo
 import com.android.internal.telephony.cdma.SignalToneUtil;
 import com.android.internal.telephony.uicc.IccCardApplicationStatus;
 import com.android.internal.telephony.uicc.IccCardStatus;
+import com.android.internal.telephony.uicc.IccUtils;
 
 /**
  * Qualcomm RIL for the Samsung msm8974 family.
@@ -247,7 +248,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
 
     @Override
     protected void
-    processUnsolicited (Parcel p) {
+    processUnsolicited (Parcel p, int type) {
         Object ret;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
         int response = p.readInt();
@@ -281,7 +282,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
                 p.setDataPosition(dataPosition);
 
                 // Forward responses that we are not overriding to the super class
-                super.processUnsolicited(p);
+                super.processUnsolicited(p, type);
                 return;
         }
 
@@ -289,7 +290,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
 
     @Override
     protected RILRequest
-    processSolicited (Parcel p) {
+    processSolicited (Parcel p, int type) {
         int serial, error;
         boolean found = false;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
@@ -321,7 +322,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
             /* Nothing we care about, go up */
             p.setDataPosition(dataPosition);
             // Forward responses that we are not overriding to the super class
-            return super.processSolicited(p);
+            return super.processSolicited(p, type);
         }
         rr = findAndRemoveRequestFromList(serial);
         if (rr == null) {
@@ -358,7 +359,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
         }
         return response;
     }
-    
+
     /**
      * Set audio parameter "wb_amr" for HD-Voice (Wideband AMR).
      *
@@ -438,7 +439,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
         send(rr);
     }
 
-    
+
     // Workaround for Samsung CDMA "ring of death" bug:
     //
     // Symptom: As soon as the phone receives notice of an incoming call, an
