3d745ebfbbbf056bdc0d0010af11f11395c0897f
Kevin F. Haggerty <haggertk@lineageos.org>
2017-04-23 09:44:08 -0600

    hlte-common: Remove obsolete BOARD_SEPOLICY_UNION
    Change-Id: I2c95a55bff1774f212f7d57b12fa8cbdeaf436e4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -93,7 +94,6 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_UNION += macloader.te
 BOARD_SEPOLICY_DIRS += device/samsung/hlte-common/sepolicy
 
 # Sensors
