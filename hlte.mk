bdb3811c319fcf1351426302510e108e3469e296
Kevin F. Haggerty <haggertk@lineageos.org>
2017-04-25 20:05:40 -0600

    hlte-common: Do not assume all variants have same NFC chip
    * Because they don't, so don't force the nfc_nci.bcm2079x HAL on them
    
    Change-Id: Iedc46c97a5f6aa30478d1f5c7eb6be2a3eaf66d9

--- hlte.mk
+++ hlte.mk
@@ -114,14 +114,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     NfcNci \
-    nfc_nci.bcm2079x.msm8974 \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml \
-    $(LOCAL_PATH)/configs/libnfc-brcm-20791b05.conf:system/etc/libnfc-brcm-20791b05.conf \
-    $(LOCAL_PATH)/configs/libnfc-brcm-20791b04.conf:system/etc/libnfc-brcm-20791b04.conf \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
+    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # Radio
 PRODUCT_PACKAGES += \
