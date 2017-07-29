a4711a8c087dda507ad35354eb59fe5dc8a3b8d0
Kevin F. Haggerty <haggertk@lineageos.org>
2017-07-29 06:56:16 -0600

    hlte-common: Add hltechn to makefile guards
    * Android.mk and setup-makefiles.sh
    
    Change-Id: If36b544d0bc7c9a3514a3f288461ecd706b8df62

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +17,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hlte hltetmo hltespr hlteusc hltevzw,$(TARGET_DEVICE)),)
+ifneq ($(filter hlte hltechn hltetmo hltespr hlteusc hltevzw,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
