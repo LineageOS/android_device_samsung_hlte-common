fb1586cace1c483285fb48ca1f078da16c530a5b
Kevin F. Haggerty <haggertk@lineageos.org>
2017-08-27 10:13:05 -0600

    Revert "bluetooth: Our kernel is missing CLOCK_BOOTTIME_ALARM (alarmtimer)"
    This reverts commit 6093dce83e6900ef19c118d81c90de7f1637f737.
    
    Change-Id: I4da335536cc79e90ca311e3c0aa3afd34d48934e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -24,6 +24,4 @@
 #define BTIF_HF_WBS_PREFERRED TRUE
 #define BLE_VND_INCLUDED TRUE
 
-/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
