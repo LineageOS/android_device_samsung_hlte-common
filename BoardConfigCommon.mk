d98d901d696584df4e315db65c0a34da5c48dca6
jrior001 <jriordan001@gmail.com>
2017-10-16 20:30:49 -0400

    hlte-common: adapt cmhw to new lineagesdk structure
    Change-Id: Ie03c5664703ec3fd954efc71de47d6340086f79d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,8 +59,7 @@ TARGET_USE_COMPAT_GRALLOC_ALIGN := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # CMHW
-BOARD_HARDWARE_CLASS += device/samsung/hlte-common/cmhw
-BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/hlte-common/lineagehw
 
 # Graphics
 TARGET_HAVE_NEW_GRALLOC := true
