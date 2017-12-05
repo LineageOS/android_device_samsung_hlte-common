0fa914dba93cf846ac0524e4e2f52f2fda948dc6
Paul Keith <javelinanddart@gmail.com>
2017-12-06 00:38:21 +0100

    hlte-common: camera: Steal klte's new wrapper
    Change-Id: I7cc1e4c3eda73b505f6520d28d856b7f0a669951

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,7 +1,8 @@
-ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
-
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
@@ -9,13 +10,8 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
-LOCAL_C_INCLUDES := \
-    system/media/camera/include
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.msm8974
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
