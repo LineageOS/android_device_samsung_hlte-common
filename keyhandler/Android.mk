d701719d7f60096b2484c5ca352e7b72638f86fc
Kevin F. Haggerty <haggertk@lineageos.org>
2018-01-21 08:55:12 -0700

    hlte-common: Rebrand KeyHandler
    Change-Id: I28314f9d5a610dfee92e83d69339abcd0010f78e

--- keyhandler/Android.mk
+++ keyhandler/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := com.cyanogenmod.keyhandler
+LOCAL_MODULE := org.lineageos.keyhandler
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_MODULE_TAGS := optional
 LOCAL_DEX_PREOPT := false
