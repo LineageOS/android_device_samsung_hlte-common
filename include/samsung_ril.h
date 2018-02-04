cfa9bb84d11229f3c6a921ccd7b065201e5d99d6
Paul Keith <javelinanddart@gmail.com>
2018-02-04 02:13:35 +0100

    hlte-common: Override samsung_ril.h
    * We need to remap RIL_UNSOL_ON_SS_LL (11055)
    
    Change-Id: Ifebc9a1c317c7a276b9be09e52ddecc62177d599

---
+++ include/samsung_ril.h
@@ -0,0 +1,27 @@
+/*
+**
+** Copyright 2018, The LineageOS Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#define RIL_UNSOL_ON_SS_LL 11055
+
+static inline void remapUnsol(int *unsol, void *data __unused, size_t *datalen __unused) {
+    switch(*unsol) {
+        case RIL_UNSOL_ON_SS_LL:
+            *unsol = RIL_UNSOL_ON_SS;
+        return;
+        default:
+    }
+}
