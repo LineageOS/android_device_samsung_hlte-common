145da6f6dc6b5c74e8140c45589c300fd6d0dc82
Kevin F. Haggerty <haggertk@lineageos.org>
2018-02-05 17:38:16 -0700

    hlte-common: Add hltekor to makefile guards
    Change-Id: I6181a5f31819757a628c2137f006ceb9ca3c3d37

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hlte hltechn hltetmo hltespr hlteusc hltevzw,$(TARGET_DEVICE)),)
+ifneq ($(filter hlte hltechn hltekor hltetmo hltespr hlteusc hltevzw,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
