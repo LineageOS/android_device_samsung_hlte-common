5783c7636cdf2c2ba5e0e5759bbe7ee940d5fd29
Paul Keith <javelinanddart@gmail.com>
2018-02-19 18:21:29 +0100

    hlte-common: Use sdfat for exfat and vfat
    Change-Id: I660c5b3008636a651a2812fe1739ff8a2ea845cb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,7 +42,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_HARDWARE_CLASS += $(COMMON_PATH)/lineagehw
 
 # Extended Filesystem Support
-TARGET_EXFAT_DRIVER := exfat
+TARGET_EXFAT_DRIVER := sdfat
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
