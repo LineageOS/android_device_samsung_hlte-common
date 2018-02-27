b8c4fcbf7ee21ed96aaceb1676e4711f73d0e1f5
Michael Bestas <mkbestas@lineageos.org>
2018-02-27 20:00:45 +0000

    hlte-common: Get rid of recovery-only fstab
     * Mounting /system partition is handled in kernel now,
       however removing the entry from fstab caused issues building
       the OTA. The workaround was to have a separate fstab, but turns out
       that simply setting the recoveryonly flag does the trick because
       those are then ignored during a normal Android boot.
    
    Change-Id: I2944384d0a1c41bc9f9f51e2e29daff2bed0a0f4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,7 +87,7 @@ BOARD_PROVIDES_LIBRIL := true
 TARGET_RIL_VARIANT := caf
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.full
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_USE_LCD_POWER_BLANK := true
 
 # SELinux
