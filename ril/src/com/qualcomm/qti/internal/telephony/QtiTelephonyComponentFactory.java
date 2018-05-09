523f7914b9bd7fcf16cddbbd32648786fc7bc74b
Paul Keith <javelinanddart@gmail.com>
2018-05-09 15:32:27 +0200

    hlte-common: Implement crude manual SIM provisioning
    Change-Id: I2965adbb589a1f7faaae8936b89b641a5cefa0eb

--- ril/src/com/qualcomm/qti/internal/telephony/QtiTelephonyComponentFactory.java
+++ ril/src/com/qualcomm/qti/internal/telephony/QtiTelephonyComponentFactory.java
@@ -32,7 +32,7 @@ public class QtiTelephonyComponentFactory extends TelephonyComponentFactory {
             Phone[] phones, CommandsInterface[] commandsInterfaces) {
         Rlog.d(LOG_TAG, "makeSecExtTelephonyClasses");
         try {
-            SecExtTelephony.init(context);
+            SecExtTelephony.init(context, phones, commandsInterfaces);
         } catch (NoClassDefFoundError e) {
             Rlog.e(LOG_TAG, "Error creating SecExtTelephony", e);
         }
