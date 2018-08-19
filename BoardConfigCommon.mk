9269da5d8538ba299e078e376e5466477b28026f
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-19 08:53:49 -0600

    DO NOT MERGE: hlte-common: Requisite bring-up BS change
    Currently:
    * Permissive
    * Kill bcm2079 NFC
    * Kill pn547 NFC
    * Don't build SamsungDoze
    * Don't build FlipFlap
    * Don't build keyhandler
    * Don't use CAF TARGET_RIL_VARIANT -- this doesn't matter now, and we
      will eventually, but once hw/ril gets guarded then we're out of luck
    
    Change-Id: Iec4e6927e019c9ec91cc16d3eeacb0990ffff6f5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,6 +50,7 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
@@ -84,7 +85,7 @@ TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 
 # Radio
 BOARD_PROVIDES_LIBRIL := true
-TARGET_RIL_VARIANT := caf
+#TARGET_RIL_VARIANT := caf
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
