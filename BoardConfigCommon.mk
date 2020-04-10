4e56738992ee80b138dcdc4d080c7e76885206e6
Kevin F. Haggerty <haggertk@lineageos.org>
2020-04-10 09:48:26 -0600

    [DNM] hlte-common: Bring-up h4x
    Change-Id: I2587846b8a47c6dbfe4fcb357e4f3738d7dea44f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,6 +46,7 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
